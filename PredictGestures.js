eaca64633344de2a0dae6c754be3aaa2efcc103e
Philip <pmccalli@uvm.edu>
2020-10-12 11:33:03 -0400

    the slow downening

--- PredictGestures.js
+++ PredictGestures.js
@@ -1,28 +1,63 @@
 let trainingCompleted = false;
+var controllerOptions = {};
 let predictedClassLabels = nj.zeros([150,1]);
 let testinteger = 0;
-let numSamples = nj.zeros([150,1]);
+let n= 0;
+//let numSamples = nj.zeros([150,1]);
 const knnClassifier = ml5.KNNClassifier();
-let testingSampleIndex = 0;
+let m = 0;
+let d = 8;
+let c = 0;
+
 
 let numFeatures = nj.zeros(150, 4);
 
 //console.log(irisData.pick(4));
 
+var numSamples = 100;
+var framesOfData = nj.zeros([5,4,6]);
+var currentSample = 0;
+id="displayArea";
+ width="200";
+ height="100";
+ style="background:#dddddd;";
 
-function draw(){
+console.log(document.getElementById("displayArea")); //this returns null something went wrong
+
+
+
+
+
+
+rawXMin = 99;
+rawYMin = 99;
+rawXMax = 1;
+rawYMax = 1;
+previousNumHands = 0;
+currentNumHands = 0;
+i = 0;
+x = 0;
+y = 0;
+z = 0;
+	
+Leap.loop(controllerOptions, function(frame){
 	//console.log("Draw");
 	//console.log(train0.shape[3]);
-	
-	
+	Leap.loop( controllerOptions, function(frame){clear();
 		if(trainingCompleted == false){
 			Train();
 			trainingCompleted = true;
 		}
-		Test();
+		currentNumHands = frame.hands.length;
+		HandleFrame(frame);
+		} );
+		previousNumHands = currentNumHands;
+			
+
 		
-		
-}
+
+
+
 function Train(){
 	for(i = 0; i < 2; i++){
 		//console.log( train0.pick(null,null,null,i).toString());
@@ -33,13 +68,16 @@ function Train(){
 		//console.log(testinteger);
 		
 		features = features.reshape(1,120);
+		knnClassifier.addExample(features.tolist(), 8);
 		features1 = features1.reshape(1,120);
+		
 		//console.log(testinteger);
 		
 		//console.log(features.toString());
-		knnClassifier.addExample(features.tolist(), 8);
-		
 		knnClassifier.addExample(features1.tolist(), 0);
+		
+		
+		
 		
 		testinteger += 1;
 		console.log(testinteger);
@@ -51,34 +89,96 @@ function Train(){
 }
 function Test(){
 	for(i = 0; i < 2; i++){
-		//console.log( test.pick(null,null,null,i).toString());
-		features = train0.pick(null,null,null,i);
-		featurest = features.reshape(1,120);
-		//console.log(featurest.toString());
-		//console.log(featurest.tolist());
+		
+		//features = test.pick(null,null,null,i);
+		featurest = framesOfData.reshape(1,120);
+		
 		predictedSign = knnClassifier.classify(featurest.tolist(),GotResults);
-		//console.log(predictedSign.toString())
+		
 	}
 }
 function GotResults(err, result){
-		//console.log(err);
-		//console.log(typeof(result));
+	if(result){
 		console.log(parseInt(result.label));
-		//console.log(result);
-		predictedClassLabels.set(testingSampleIndex, 0, parseInt(result.label));
-		//console.log(testingSampleIndex);
-		testingSampleIndex += 1;
-		if(testingSampleIndex >= 150){
-			testingSampleIndex = 1;
-		}
-		//console.log(testingSampleIndex);
-		//console.log(testingSampleIndex, parseInt(result.label));
+		c = parseInt(result.label);
+		n += 1;
+		m = (((n - 1)* m + (parseInt(result.label) == d))/ n); 
+		//console.log(err);
+		consle.log(n,m,c);
+	}
+	else{
+		console.log('no REsult');
+	}		
+		//console.log(predictedClassLabels.toString());
 	
 	
 }
+// upon adding leap loop code, html when run slowed considerably.
+// upon checking average, html refused to print average, and after a short while
+// black screened the broswer window, and then said too many errors had been reported.
+//wondering if something went wrong with leap console
+function HandleFrame(frame){
+	
+	if(frame.hands.length > 0){
+		hand = frame.hands[0];
+		var interactionBox = frame.interactionBox;
+		HandleHand(hand, interactionBox);
+		Test();
+		//console.log(framesOfData.toString());
+	}
+	else {
+		//circle(width/2, height/2, 50);
+	}
+}
+function HandleHand(hand,interactionBox){
+	
+	let fingers = hand['fingers'];
+	for (i in fingers){
+		let fingerIndex = i;
+		let finger = fingers[i];
+		HandleFinger(finger, fingerIndex, interactionBox)
+	}
+}
+
+function HandleFinger(finger, fingerIndex, interactionBox){
+	
+	let bones = finger['bones'];
+	for(i in bones){
+		let boneIndex = i;
+		let bone = bones[i];
+		//strokeWeight(5 - i);
+		/*if(currentNumHands == 1){
+			stroke(0,255 - (i*40),0);
+		}
+		else{
+			stroke(255 - (i*40),0,0);
+		}*/
+		strokeWeight(5 - i);
+		stroke(180 - (i*40));
+		HandleBone(bone, fingerIndex, boneIndex, interactionBox)
+	}
+}
+function HandleBone(bone, fingerIndex, boneIndex, interactionBox){
+	normalizedPrevJoint = interactionBox.normalizePoint(bone['prevJoint'], true);
+	normalizedNextJoint = interactionBox.normalizePoint(bone['nextJoint'], true)
+	let boneStartX = normalizedPrevJoint[0];
+	let boneStartY = normalizedPrevJoint[1];
+	let boneStartZ = normalizedPrevJoint[2];
+	let boneEndX = normalizedNextJoint[0];
+	let boneEndY = normalizedNextJoint[1];
+	let boneEndZ = normalizedNextJoint[2];
+	framesOfData.set(fingerIndex, boneIndex, 0, boneStartX);
+	framesOfData.set(fingerIndex, boneIndex, 1, boneStartY);
+	framesOfData.set(fingerIndex, boneIndex, 2, boneStartZ);
+	framesOfData.set(fingerIndex, boneIndex, 3, boneEndX);
+	framesOfData.set(fingerIndex, boneIndex, 4, boneEndY);
+	framesOfData.set(fingerIndex, boneIndex, 5, boneEndZ);
+	
+	line(normalizedPrevJoint[0] * window.innerWidth, (1 - normalizedPrevJoint[1])* window.innerHeight, normalizedNextJoint[0]*window.innerWidth, (1 - normalizedNextJoint[1])*window.innerHeight);
+}
 
 
-
+});
 
 
 
--- Record.js
+++ Record.js
@@ -1,8 +1,7 @@
 nj.config.printThreshold = 1000;
 var controllerOptions = {};
-var numSamples = 2;
+var numSamples = 100;
 var framesOfData = nj.zeros([5,4,6, numSamples]);
-//var framesOfDatatemp = nj.zeros([5,4,6, numSamples]);
 var currentSample = 0;
 id="displayArea";
  width="200";
@@ -13,26 +12,7 @@ console.log(document.getElementById("displayArea")); //this returns null somethi
 
 
 
-/*var controller = new Leap.Controller();
-controller.on("frame", function(frame){
-    //if(frame.pointables.length > 0)
-    //{
-        //canvasElement.width = window.innerWidth; //clear
-        
-        //Get a pointable and normalize the tip position
-        var pointable = frame.pointables[0];
-        var interactionBox = frame.interactionBox;
-        var normalizedPosition = interactionBox.normalizePoint(pointable.tipPosition, true);
-        
-        // Convert the normalized coordinates to span the canvas
-        var canvasX = window.innerWidth * normalizedPosition[0];
-		var canvasY = window.innerHeight * (1 - normalizedPosition[1]);
-        //we can ignore z for a 2D context
-        
-       // displayArea.strokeText("(" + canvasX.toFixed(1) + ", " + canvasY.toFixed(1) + ")", canvasX, canvasY);
-    //}
-});
-controller.connect();*/
+
 
 
 rawXMin = 99;
@@ -46,7 +26,7 @@ x = 0;
 y = 0;
 z = 0;
 function HandleFrame(frame){
-	//console.log(frame.hands);
+	
 	if(frame.hands.length > 0){
 		hand = frame.hands[0];
 		var interactionBox = frame.interactionBox;
@@ -79,42 +59,18 @@ function HandleFinger(finger, fingerIndex, interactionBox){
 		else{
 			stroke(255 - (i*40),0,0);
 		}
-		//console.log(fingerIndex, boneIndex, currentSample);
 		HandleBone(bone, fingerIndex, boneIndex, interactionBox)
-		
-			
-		
-		
 	}
-	
-	
-	
 }
 function HandleBone(bone, fingerIndex, boneIndex, interactionBox){
-	//console.log(bone);
 	normalizedPrevJoint = interactionBox.normalizePoint(bone['prevJoint'], true);
 	normalizedNextJoint = interactionBox.normalizePoint(bone['nextJoint'], true)
 	let boneStartX = normalizedPrevJoint[0];
-	//console.log(bone['prevJoint']);
 	let boneStartY = normalizedPrevJoint[1];
 	let boneStartZ = normalizedPrevJoint[2];
-	
 	let boneEndX = normalizedNextJoint[0];
 	let boneEndY = normalizedNextJoint[1];
 	let boneEndZ = normalizedNextJoint[2];
-	//console.log( framesOfData.pick(null,null,null,1).toString() );
-	
-	//console.log(boneStartX);
-	//console.log(normalizedNextJoint, normalizedPrevJoint);
-	//console.log(bone['prevJoint'])
-	//console.log(boneStartX);
-	//console.log(boneEndX);
-	//console.log(boneStartY);
-	//console.log(boneEndY);
-	//console.log(boneStartZ);
-	//console.log(boneEndZ);
-	
-	//temp = boneEndX + boneStartX + boneEndY + boneStartY + boneEndZ + boneStartZ;
 	framesOfData.set(fingerIndex, boneIndex, 0, currentSample, boneStartX);
 	framesOfData.set(fingerIndex, boneIndex, 1, currentSample, boneStartY);
 	framesOfData.set(fingerIndex, boneIndex, 2, currentSample, boneStartZ);
@@ -122,41 +78,9 @@ function HandleBone(bone, fingerIndex, boneIndex, interactionBox){
 	framesOfData.set(fingerIndex, boneIndex, 4, currentSample, boneEndY);
 	framesOfData.set(fingerIndex, boneIndex, 5, currentSample, boneEndZ);
 	line(normalizedPrevJoint[0] * window.innerWidth, (1 - normalizedPrevJoint[1])* window.innerHeight, normalizedNextJoint[0]*window.innerWidth, (1 - normalizedNextJoint[1])*window.innerHeight);
-	//console.log(start,end)
-	/*
-	let basis = bone['basis'];
-	
-	let tip = basis[0];
-	x = tip[0];
-	y = tip[1];
-	z = tip[2];
-	//console.log(bone);
-	//console.log(tip);
-	*/
-	
-	
-
-	
-	
-
-	
-	/*if(rawXMin > x){
-		rawXMin = x;
-	}
-	if(rawYMin < y){
-		rawYMin = y;
-	}
-	if(rawXMax < x){
-		rawXMax = x;
-	}
-	if(rawYMax > y){
-		rawYMax = y;
-	}*/
-	
 }	
 function RecordData(){
 	if(currentNumHands == 2){
-		//console.log(currentSample);
 		currentSample = currentSample + 1;
 		if(currentSample == numSamples){
 			currentSample = 0;
@@ -166,53 +90,20 @@ function RecordData(){
 		background(0);
 		
 		//console.log('***');
-		//console.log(framesOfData.toString());
-		console.log( framesOfData.pick(null,null,null,1).toString() );
+		console.log(framesOfData.toString());
+		//console.log( framesOfData.pick(null,null,null,1).toString() );
 		
 	}	
 }
-//my second 3d tensor has a lot of zeros, i don't know how to make a loop that records 100 frames of data.
-//second 3d tensor fills when numSamples is odd
-//TA says post incorrect work in blackboard, ask Professor Bongard for extension
-	
-	
-
-/*function TransformCoordinates(x,y){
-	let rawXRange = rawXMax - rawXMin; // total range the finger has to move 
-	let rawYRange = rawYMax - rawYMin;
-	
-	let xShift = x - rawXMin; // how far x is away from the min of x
-	let yShift = y - rawYMin;
-	
-	let xRatio = xShift/rawXRange;
-	let yRatio = yShift/rawYRange;
-	//console.log(xRatio, yRatio);
-	
-	let canvasX = xRatio * width;
-	let canvasY = yRatio * height;
-	return [canvasX,canvasY]
-}*/
-
-
 
 Leap.loop(controllerOptions, function(frame) {
 	
 	clear();
 	currentNumHands = frame.hands.length;
 	HandleFrame(frame);
-	RecordData()
+	RecordData();
 	previousNumHands = currentNumHands;
+
 	
-	
-	//p = Math.floor(Math.random()*2) == 1 ? 1 : -1
-	//g = Math.floor(Math.random()*2) == 1 ? 1 : -1
-	//console.log(i)
-	
-	
-	
-		//console.log(fingers);
-		
-	
-	
-	
+
 });
